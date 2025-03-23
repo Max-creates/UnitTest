ec015cc5dbbdb9dc3ec72728cca1fc65e8b38af9
Maksym Solomyanov <maksym.solomyanov@gmail.com>
2025-03-23 21:32:56 +0100

    Add project files.

---
+++ EnumerableExtensions.cs
@@ -0,0 +1,7 @@
+﻿public static class EnumerableExtensions
+{
+    public static int SumOfEvenNumbers(this IEnumerable<int> numbers)
+    {
+        return numbers.Where(n => n % 2 == 0).Sum();
+    }
+}
